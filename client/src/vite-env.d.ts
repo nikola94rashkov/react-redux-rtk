8bef5e7645a361804f5f066c166b2776c1cfc206
Nikola Rashkov <nikola94rashkov@gmail.com>
2025-05-09 15:15:55 +0300

    :recycle: (Remove unnecessary files) none

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,4 @@
 /// <reference types="vite/client" />
+VITE_API_BASE_URL = 'http://localhost:4000';
+VITE_API_AUTH_URL = 'http://localhost:4000/api/auth';
+VITE_API_POSTS_URL = 'http://localhost:4000/api/posts';
