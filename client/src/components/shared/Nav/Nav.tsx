b93f85e6d27185e94c30dea20165717e0f9bdebf
Nikola Rashkov <nikola94rashkov@gmail.com>
2025-05-23 22:13:52 +0300

    :sparkles: (Add nav modificationa if user is loged or not) none

--- client/src/components/shared/Nav/Nav.tsx
+++ client/src/components/shared/Nav/Nav.tsx
@@ -5,25 +5,65 @@ import {
 } from '@/components/ui/navigation-menu.tsx'
 
 import { NavLinkButton } from '@/components/shared'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/store/store.ts'
 
 export const Nav = () => {
-	return (
-		<NavigationMenu>
-			<NavigationMenuList>
-				<NavigationMenuItem>
-					<NavLinkButton
-						text='Home'
-						href='/'
-					/>
-				</NavigationMenuItem>
+	const { user } = useSelector((state: RootState) => state.authSlice)
 
-				<NavigationMenuItem className='font-bold p-2 pl-3 pr-3 bg-amber-50 rounded-2xl hover:bg-amber-100 active:bg-amber-100'>
-					<NavLinkButton
-						text='Dashboard'
-						href='/dashboard'
-					/>
-				</NavigationMenuItem>
-			</NavigationMenuList>
-		</NavigationMenu>
+	return (
+		<>
+			{user ? (
+				<NavigationMenu>
+					<NavigationMenuList>
+						<NavigationMenuItem>
+							<NavLinkButton
+								text='Home'
+								href='/'
+							/>
+						</NavigationMenuItem>
+
+						<NavigationMenuItem className='font-bold p-2 pl-3 pr-3 bg-amber-50 rounded-2xl hover:bg-amber-100 active:bg-amber-100'>
+							<NavLinkButton
+								text='Dashboard'
+								href='/dashboard'
+							/>
+						</NavigationMenuItem>
+
+						<NavigationMenuItem className='font-bold p-2 pl-3 pr-3 bg-amber-50 rounded-2xl hover:bg-amber-100 active:bg-amber-100'>
+							<NavLinkButton
+								text='Logout'
+								href='/logout'
+							/>
+						</NavigationMenuItem>
+					</NavigationMenuList>
+				</NavigationMenu>
+			) : (
+				<NavigationMenu>
+					<NavigationMenuList>
+						<NavigationMenuItem>
+							<NavLinkButton
+								text='Home'
+								href='/'
+							/>
+						</NavigationMenuItem>
+
+						<NavigationMenuItem className='font-bold p-2 pl-3 pr-3 bg-amber-50 rounded-2xl hover:bg-amber-100 active:bg-amber-100'>
+							<NavLinkButton
+								text='Login'
+								href='/login'
+							/>
+						</NavigationMenuItem>
+
+						<NavigationMenuItem className='font-bold p-2 pl-3 pr-3 bg-amber-50 rounded-2xl hover:bg-amber-100 active:bg-amber-100'>
+							<NavLinkButton
+								text='Register'
+								href='/register'
+							/>
+						</NavigationMenuItem>
+					</NavigationMenuList>
+				</NavigationMenu>
+			)}
+		</>
 	)
 }
