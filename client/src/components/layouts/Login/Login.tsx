4e286ffa98601625fe9361b04fb648f232c41fc4
Nikola Rashkov <nikola94rashkov@gmail.com>
2025-06-04 13:19:42 +0300

    :sparkles: (Add Card component and implement it in Login layout) none

--- client/src/components/layouts/Login/Login.tsx
+++ client/src/components/layouts/Login/Login.tsx
@@ -1,24 +1,31 @@
-import { Button, Input, Section } from '@/components'
+import { Link, useNavigate } from 'react-router-dom'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useDispatch } from 'react-redux'
+import { toast } from 'sonner'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useLoginMutation } from '@/store/user/userApiSlice.ts'
+import { setUser } from '@/store/auth/authSlice.ts'
 import {
+	defaultValues,
+	FormFields,
+	schema,
+} from '@/components/layouts/Login/Schema.ts'
+import {
+	Button,
+	Input,
+	Section,
 	Form,
 	FormControl,
 	FormField,
 	FormItem,
 	FormLabel,
 	FormMessage,
-} from '@/components/ui/form.tsx'
-import { useNavigate } from 'react-router-dom'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import {
-	defaultValues,
-	FormFields,
-	schema,
-} from '@/components/layouts/Login/Schema.ts'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { toast } from 'sonner'
-import { useLoginMutation } from '@/store/user/userApiSlice.ts'
-import { useDispatch } from 'react-redux'
-import { setUser } from '@/store/auth/authSlice.ts'
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components'
 
 export const Login = () => {
 	const dispatch = useDispatch()
@@ -70,46 +77,74 @@ export const Login = () => {
 
 	return (
 		<Section>
-			<Form {...form}>
-				<form
-					onSubmit={handleSubmit(onSubmit)}
-					className='space-y-8'>
-					<FormField
-						control={control}
-						name='email'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>email</FormLabel>
-								<FormControl>
-									<Input {...field} />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={control}
-						name='password'
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>password</FormLabel>
-								<FormControl>
-									<Input
-										type='password'
-										{...field}
+			<Card>
+				<CardHeader>
+					<CardTitle>Login to your account</CardTitle>
+					<CardDescription>
+						Enter your email below to login to your account
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<Form {...form}>
+						<form onSubmit={handleSubmit(onSubmit)}>
+							<div className='flex flex-col gap-6'>
+								<div className='grid gap-3'>
+									<FormField
+										control={control}
+										name='email'
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>Email</FormLabel>
+												<FormControl>
+													<Input
+														{...field}
+														placeholder='m@example.com'
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
 									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<Button
-						type='submit'
-						disabled={isSubmitting}>
-						Login
-					</Button>
-				</form>
-			</Form>
+								</div>
+								<div className='grid gap-3'>
+									<FormField
+										control={control}
+										name='password'
+										render={({ field }) => (
+											<FormItem>
+												<FormLabel>password</FormLabel>
+												<FormControl>
+													<Input
+														type='password'
+														{...field}
+													/>
+												</FormControl>
+												<FormMessage />
+											</FormItem>
+										)}
+									/>
+								</div>
+								<div className='flex flex-col gap-3'>
+									<Button
+										type='submit'
+										className='w-full'
+										disabled={isSubmitting}>
+										Login
+									</Button>
+								</div>
+							</div>
+							<div className='mt-4 text-center text-sm'>
+								Don&apos;t have an account?
+								<Link
+									className='underline underline-offset-4'
+									to='/register'>
+									Sign up
+								</Link>
+							</div>
+						</form>
+					</Form>
+				</CardContent>
+			</Card>
 		</Section>
 	)
 }
